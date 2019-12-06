b6eb5042ecf9af10bc829da50c7307ad1fe1f4fa
WSH <mikko5269@gmail.com>
2019-12-06 21:43:48 +0800

    WSH

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MyWorks.Models;
+
+namespace MyWorks.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [Route("")]
+        [Route("Home")]
+        [Route("Home/Index")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("Home/AspNetCore/ShopMap")]
+        public IActionResult ShopMap()
+        {
+            return View();
+        }
+
+        [Route("Home/Designer")]
+        public IActionResult Designer()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Controllers/ShopMapCoreController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyWorks.Controllers
+{
+    public class ShopMapCoreController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
